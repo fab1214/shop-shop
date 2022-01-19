f2d710599e30db73b09f42ffe70bbb4c9584be1c
fab1214 <88295561+fab1214@users.noreply.github.com>
2022-01-19 17:43:26 -0500

    stripe added

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,22 +1,38 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import CartItem from "../CartItem";
 import Auth from "../../utils/auth";
 import { useStoreContext } from "../../utils/GlobalState";
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
-import {idbPromise} from '../../utils/helpers';
+import { idbPromise } from "../../utils/helpers";
+import { QUERY_CHECKOUT } from "../../utils/queries";
+import { loadStripe } from "@stripe/stripe-js";
+import { useLazyQuery } from "@apollo/client";
 import "./style.css";
+
+const stripePromise = loadStripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
 
 const Cart = () => {
   //use custom useStoreContext() hook to establish a state variable and dispatch function
   //to update the state of the cart
   const [state, dispatch] = useStoreContext();
+  //useLazyQuery is used to render component based on a user action (like button click)
+  const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+
+  //Redirect to stripe checkout once receiving sessionId
+  useEffect(() => {
+    if (data) {
+      stripePromise.then((res) => {
+        res.redirectToCheckout({ sessionId: data.checkout.session });
+      });
+    }
+  }, [data]);
 
   useEffect(() => {
     async function getCart() {
-      const cart = await idbPromise('cart', 'get');
+      const cart = await idbPromise("cart", "get");
       dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
-    };
-    
+    }
+
     //if state.cart.length = 0, execute getCart to retrieve key valur pairs from IndexedDB and drop everything in the cart again
     if (!state.cart.length) {
       getCart();
@@ -35,6 +51,21 @@ const Cart = () => {
     return sum.toFixed(2);
   }
 
+  function submitCheckout() {
+    const productIds = [];
+
+    //for each item in cart
+    state.cart.forEach((item) => {
+      //loop through items in cart and push ids into productIds array
+      for (let i = 0; i < item.purchaseQuantity; i++) {
+        productIds.push(item._id);
+      }
+    });
+
+    getCheckout({
+      variables: { products: productIds },
+    });
+  }
   //if cartOpen state is false, close cart and show cart icon
   if (!state.cartOpen) {
     return (
@@ -45,42 +76,37 @@ const Cart = () => {
       </div>
     );
   }
-  
+
   return (
     <div className="cart">
-    <div className="close" onClick={toggleCart}>[close]</div>
-    <h2>Shopping Cart</h2>
-    {state.cart.length ? (
-      <div>
-        {state.cart.map(item => (
-          <CartItem key={item._id} item={item} />
-        ))}
-        <div className="flex-row space-between">
-          <strong>Total: ${calculateTotal()}</strong>
-          {
-            Auth.loggedIn() ?
-              <button>
-                Checkout
-              </button>
-              :
-              <span>(log in to check out)</span>
-          }
-        </div>
+      <div className="close" onClick={toggleCart}>
+        [close]
       </div>
-    ) : (
-      <h3>
-        <span role="img" aria-label="shocked">
-          😱
-        </span>
-        You haven't added anything to your cart yet!
-      </h3>
-    )}
-  </div>
-  
+      <h2>Shopping Cart</h2>
+      {state.cart.length ? (
+        <div>
+          {state.cart.map((item) => (
+            <CartItem key={item._id} item={item} />
+          ))}
+          <div className="flex-row space-between">
+            <strong>Total: ${calculateTotal()}</strong>
+            {Auth.loggedIn() ? (
+              <button onClick={submitCheckout}>Checkout</button>
+            ) : (
+              <span>(log in to check out)</span>
+            )}
+          </div>
+        </div>
+      ) : (
+        <h3>
+          <span role="img" aria-label="shocked">
+            😱
+          </span>
+          You haven't added anything to your cart yet!
+        </h3>
+      )}
+    </div>
   );
-
-
 };
-
 
 export default Cart;
